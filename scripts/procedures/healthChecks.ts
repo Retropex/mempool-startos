751dfbb9c70901afd52b0eed59106fabaf6447c3
Léo Haf <leo@haf.ovh>
2025-04-14 14:12:47 +0200

    fix DNS

--- scripts/procedures/healthChecks.ts
+++ scripts/procedures/healthChecks.ts
@@ -3,7 +3,7 @@ import { types as T, checkWebUrl, catchError } from "../deps.ts";
 export const health: T.ExpectedExports.health = {
   // deno-lint-ignore require-await
   async "web-ui"(effects, _duration) {
-    return checkWebUrl("http://mempool.embassy:8080")(effects, 30000).catch(
+    return checkWebUrl("http://mempooldotguide.embassy:8080")(effects, 30000).catch(
       catchError(effects)
     );
   },
